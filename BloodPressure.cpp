3369750f6b73a4bf6d60069f286a74d67bc8caa7
breadan alaa <109834067+breadan@users.noreply.github.com>
2022-11-09 20:27:39 +0200

    Add files via upload
    You are given a person's systolic blood pressure, AA, and diastolic blood pressure, BB.
    Find the mean arterial pressure, CC, which we define as follows:

---
+++ BloodPressure.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+using namespace std;
+
+/*You are given a person's systolic blood pressure, 
+AA, and diastolic blood pressure, BB.
+Find the mean arterial pressure, CC,
+which we define as follows:
+
+C = \frac{A-B}{3} +BC= 
+3
+A−B
+​
+ +B.*/
+int main()
+{
+	double a, b;
+	double c = 0;
+	cin >> a >> b;
+	c = ((a - b) / 3) + b;
+	cout << c;
+
+
+
+
+
+
+	return 0;
+}
